e23d6f35f12d7ba894fb3e6219b67081c52cfb15
Stefanie Beutke <s.beutke@aisler.net>
2018-09-09 15:27:43 +0200

    first attempt to get selected text with window.getSelection() - currently returns empty string

--- transadd.js
+++ transadd.js
@@ -1,1 +1,20 @@
-alert("Hello World");
+/*document.addEventListener("mouseover", function(e){
+  var curTag = e.target;
+  var text = curTag.innerText;
+  var words = text.split(" ");
+
+  words.forEach(function(){
+
+  });
+
+  console.log(curTag);
+}, false);*/
+var script = document.createElement('script');
+
+script.src = 'https://code.jquery.com/jquery-1.6.3.js';
+document.getElementsByTagName('head')[0].appendChild(script);
+
+document.addEventListener("click", function(e){
+  console.log("."+document.selection.createRange().text);
+  //alert(e.target);
+});
